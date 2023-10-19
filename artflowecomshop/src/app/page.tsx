6ba483af12be6792937de423efc8916a320b40cf
Ben Allen <benjaminaallenjr@gmail.com>
2023-10-19 19:53:12 -0400

    setups

---
+++ artflowecomshop/src/app/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-3xl text-center justify-center text-blue-500 flex flex-col mt-4 px-3 py-6">Hello You </h1>
+    </main>
+  )
+}
