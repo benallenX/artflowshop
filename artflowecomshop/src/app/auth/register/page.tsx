4e91841f85ea73f683dc9dcb025d22bb733ef01e
Ben Allen <benjaminaallenjr@gmail.com>
2023-10-21 17:38:28 -0400

    register part 1

---
+++ artflowecomshop/src/app/auth/register/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const pages = () => {
+  return (
+    <div className='grid grid-cols-2 min-h-screen'>
+      <div className='h-full bg-black flex items-center justify-center'>
+        <h1 className='text text-7xl font-extrabold text-orange-500'>Artflowgorithm</h1>
+        <br />
+        <h1 className=' text text-6xl font-bold text-red-400'>~</h1>
+        <h1 className='text text-7xl font-extrabold text-white'>Shop</h1>
+      </div>
+      <div></div>
+    </div>
+  )
+}
+
+export default pages
